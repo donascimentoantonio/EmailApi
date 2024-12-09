78d413ba422b5444c1dfb2ef313229454f51fad9
Cristiano Antonio <cristianodonascimento256@gmail.com>
2024-12-09 20:27:46 -0300

    atualizando quartz para pegar das configurações

---
+++ EmailManagement.Infrastructure/Jobs/EmailBackgroundJob.cs
@@ -0,0 +1,27 @@
+﻿using EmailManagement.Domain.Services;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace EmailManagement.Infrastructure.Jobs
+{
+    public class EmailBackgroundJob : IJob
+    {
+        private readonly ILogger<EmailBackgroundJob> _logger;
+        private readonly IEmailService _emailService;
+
+        public EmailBackgroundJob(
+            ILogger<EmailBackgroundJob> logger,
+            IEmailService emailService)
+        {
+            _logger = logger;
+            _emailService = emailService;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("{UtcNow} Execução dos emails pendentes", DateTime.UtcNow);
+
+            await _emailService.SendPendingEmailsAsync();
+        }
+    }
+}
---
+++ EmailManagement.Infrastructure/Jobs/EmailBackgroundJobSetup.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.Extensions.Options;
+using Quartz;
+
+namespace EmailManagement.Infrastructure.Jobs
+{
+    public class EmailBackgroundJobSetup : IConfigureOptions<QuartzOptions>
+    {
+        public void Configure(QuartzOptions options)
+        {
+            var jobKey = JobKey.Create(nameof(EmailBackgroundJob));
+            options
+                .AddJob<EmailBackgroundJob>(jobBuilder => 
+                    jobBuilder.WithIdentity(jobKey)
+                                .DisallowConcurrentExecution()
+                    )
+                .AddTrigger(trigger =>
+                    trigger
+                        .ForJob(jobKey)
+                        .WithSimpleSchedule(schedule =>
+                            schedule.WithIntervalInMinutes(3).RepeatForever()));
+        }
+    }
+}
